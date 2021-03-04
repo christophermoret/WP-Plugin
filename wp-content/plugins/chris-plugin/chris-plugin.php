1593aae778a1da01b223e5e500e43fdcf8b9ad32
Your <christophermoret@gmail.com>
2021-03-04 15:10:54 +0100

    Adding some comments

--- wp-content/plugins/chris-plugin/chris-plugin.php
+++ wp-content/plugins/chris-plugin/chris-plugin.php
@@ -32,6 +32,7 @@
     Copyright 2005-2015 Automattic, Inc.
  */
 
+
  defined ('ABSPATH') or die ('Hey you cannot access this!');
 
  /*
@@ -128,14 +129,16 @@ function salcodes_cta( $atts ) {
 
 add_shortcode( 'boxed', 'salcodes_boxed' );
 
-function salcodes_boxed( $atts, $content = null, $tag = '' ) {
+function salcodes_boxed( $atts, $content = '$image', $tag = '' ) {
  $a = shortcode_atts( array(
  'title' => 'Title',
  'title_color' => 'white',
  'color' => 'blue',
+ 'image' => 'image',
  ), $atts );
  
  $output = '<div class="salcodes-boxed" style="border:2px solid ' . esc_attr( $a['color'] ) . ';">'.'<div class="salcodes-boxed-title" style="background-color:' . esc_attr( $a['color'] ) . ';"><h3 style="color:' . esc_attr( $a['title_color'] ) . ';">' . esc_attr( $a['title'] ) . '</h3></div>'.'<div class="salcodes-boxed-content"><p>' . esc_attr( $content ) . '</p></div>'.'</div>';
  
  return $output;
 }
+
