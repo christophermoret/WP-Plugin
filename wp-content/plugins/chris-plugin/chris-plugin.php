e1bbe99288e1585d3eab86c980039795fb87affd
Your <christophermoret@gmail.com>
2021-03-04 13:01:48 +0100

    .

--- wp-content/plugins/chris-plugin/chris-plugin.php
+++ wp-content/plugins/chris-plugin/chris-plugin.php
@@ -120,3 +120,22 @@ function salcodes_cta( $atts ) {
  $output = '<p><a href="' . esc_url( $a['link'] ) . '" id="' . esc_attr( $a['id'] ) . '" class="button ' . esc_attr( $a['color'] ) . ' ' . esc_attr( $a['size'] ) . '" target="' . esc_attr($a['target']) . '">' . esc_attr( $a['label'] ) . '</a></p>';
  return $output;
 }
+
+/**
+ * [boxed] returns the HTML code for a content box with colored titles.
+ * @return string HTML code for boxed content
+*/
+
+add_shortcode( 'boxed', 'salcodes_boxed' );
+
+function salcodes_boxed( $atts, $content = null, $tag = '' ) {
+ $a = shortcode_atts( array(
+ 'title' => 'Title',
+ 'title_color' => 'white',
+ 'color' => 'blue',
+ ), $atts );
+ 
+ $output = '<div class="salcodes-boxed" style="border:2px solid ' . esc_attr( $a['color'] ) . ';">'.'<div class="salcodes-boxed-title" style="background-color:' . esc_attr( $a['color'] ) . ';"><h3 style="color:' . esc_attr( $a['title_color'] ) . ';">' . esc_attr( $a['title'] ) . '</h3></div>'.'<div class="salcodes-boxed-content"><p>' . esc_attr( $content ) . '</p></div>'.'</div>';
+ 
+ return $output;
+}
